e268faf45c287699aeeb32bc2b60430016b2695c
Spencer <dresmanns@gmail.com>
2022-04-17 23:24:51 -0400

    hi

---
+++ app.js
@@ -0,0 +1,43 @@
+var a = 13;
+var b = 26;
+
+function addSum(params1, prams2) {
+    return addSum(params1 , prams2);
+}
+console.log(a + b)
+
+var add = (a,b) =>{
+    return add(params1, prams2);
+}
+console.log(a + b)
+
+function subSum(params1, prams2) {
+    return subSum(params1 , prams2);
+}
+console.log(a - b)
+
+var sub = (a,b) =>{
+    return sub(params1, prams2);
+}
+
+console.log(a - b)
+function mulSum(params1, prams2) {
+    return mulSum(params1 , prams2);
+}
+console.log(a * b)
+
+var mul = (a,b) =>{
+    return mul(params1, prams2);
+}
+console.log(a * b)
+
+function divSum(params1, prams2) {
+    return divSum(params1 , prams2);
+}
+console.log(a / b)
+
+var div = (a,b) =>{
+    return div(params1, prams2);
+}
+console.log(a / b)
+
